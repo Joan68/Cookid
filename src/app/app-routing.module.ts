7d04960656d729b9a3a9ffc5436919259cd2ee15
Lucas Pantchenko <lucas.pantchenko@orange.fr>
2020-06-24 11:01:13 +0200

    add routing aking name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FirstPageComponent } from './first-page/first-page.component';
+import { AskingnameComponent } from './askingname/askingname.component';
 
 
 const routes: Routes = [
   { path: 'first', component: FirstPageComponent },
+  { path: 'askingname', component: AskingnameComponent },
 ];
 
 @NgModule({
