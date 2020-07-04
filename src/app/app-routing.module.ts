31b06b0a0f70493c364ab034705f389aa5c6d4b0
Joan <joan.frey68@gmail.com>
2020-07-04 14:39:37 +0200

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { FirstPageComponent } from './first-page/first-page.component';
-import { AskingnameComponent } from './askingname/askingname.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeComponent } from './recipe/recipe.component';
+import {FirstPageComponent} from './first-page/first-page.component';
+import {AskingnameComponent} from './askingname/askingname.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {RecipeListComponent} from './recipe-list/recipe-list.component';
+import {RecipeComponent} from './recipe/recipe.component';
 
 
 const routes: Routes = [
-  { path: '', component: FirstPageComponent },
-  { path: 'askingname', component: AskingnameComponent },
-  { path: 'welcome', component: WelcomeComponent },
-  { path: 'recipe-list', component: RecipeListComponent },
-  { path: 'recipe', component: RecipeComponent },
+  {path: '', component: FirstPageComponent},
+  {path: 'askingname', component: AskingnameComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'recipe-list', component: RecipeListComponent},
+  {path: 'recipe', component: RecipeComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'cookid';
+
+  onActivate(event) {
+    window.scroll(0,0);
+    //or document.body.scrollTop = 0;
+    //or document.querySelector('body').scrollTo(0,0)
+  }
 }
